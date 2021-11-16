f4092d3c36cf239ba6e3eb7bbfca7d2238423d8a
Lisander-GitKraken <jobforandroid@protonmail.com>
2021-11-16 14:00:18 +0600

    Иницианизированы ID кнопок и STRING. Картинка и цвет кнопок - оранженый. Фон Черный

--- app/src/main/java/com/jobforandroid/geekbrains_calculator/MainActivity.java
+++ app/src/main/java/com/jobforandroid/geekbrains_calculator/MainActivity.java
@@ -3,8 +3,11 @@ package com.jobforandroid.geekbrains_calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
